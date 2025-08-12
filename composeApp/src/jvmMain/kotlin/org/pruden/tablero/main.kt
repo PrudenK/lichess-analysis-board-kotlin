8adc7296c7e6f82ab1fb759ece1945d4a7456cfe
Pruden <prudencosta@gmail.com>
2025-08-12 12:55:55 +0200

    State

--- composeApp/src/jvmMain/kotlin/org/pruden/tablero/main.kt
+++ composeApp/src/jvmMain/kotlin/org/pruden/tablero/main.kt
@@ -1,12 +1,15 @@
 package org.pruden.tablero
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "TableroAjedrez",
+        state = rememberWindowState(width = 1400.dp, height = 1000.dp)
     ) {
         App()
     }
