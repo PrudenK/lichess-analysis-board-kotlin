8cb6e7aba38bf52f46e14444c5f74fbda4314c0e
Pruden <prudencosta@gmail.com>
2025-08-16 19:39:56 +0200

    Refactor

--- composeApp/src/jvmMain/kotlin/org/pruden/tablero/components/ChessBoard.kt
+++ composeApp/src/jvmMain/kotlin/org/pruden/tablero/components/ChessBoard.kt
@@ -10,11 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.pruden.tablero.globals.Globals
-import org.pruden.tablero.models.LastMove
 import org.pruden.tablero.utils.CastleHandler
-import org.pruden.tablero.utils.CellHandler
+import org.pruden.tablero.utils.ChessBoardActionHandler
 import org.pruden.tablero.utils.History
-import org.pruden.tablero.utils.MoveCalculator
 import org.pruden.tablero.utils.PromotionHandler
 
 
@@ -37,79 +35,31 @@ fun ChessBoard() {
                                     var promoteDone = false
                                     var promoteCanceled = false
 
-
-
-                                    // Checks
-
-
-
-
-
-
-
                                     if (Globals.isWhitePromotion.value || Globals.isBlackPromotion.value) {
                                         promoteCanceled = PromotionHandler.handlePromotionClick(clickedRow, clickedCol)
                                         promoteDone = true
                                         Globals.refreshBoard.value = !Globals.refreshBoard.value
                                     }
 
-                                    // Realizar movimiento
                                     if(!promoteDone) {
                                         if (Globals.possibleMoves.value.contains(Pair(clickedCol, clickedRow))) {
-                                            val selectedPiecePos = Globals.chessBoard
-                                                .flatMapIndexed { rowIndex, row ->
-                                                    row.mapIndexedNotNull { colIndex, box ->
-                                                        if (box.pieceOnBox?.isSelected?.value == true) Pair(colIndex, rowIndex) else null
-                                                    }
-                                                }
-                                                .firstOrNull()
+                                            val selectedPiecePos = ChessBoardActionHandler.getSelectedPiecePos()
 
                                             if (selectedPiecePos != null) {
                                                 moveWasDone = true
                                                 val (selCol, selRow) = selectedPiecePos
 
-
                                                 History.push(selCol, selRow, clickedCol, clickedRow)
-                                                Globals.lastPieceStartPos = Globals.chessBoard[selRow][selCol].pieceOnBox!!.position
 
+                                                ChessBoardActionHandler.saveLastMove(clickedRow, clickedCol, selRow, selCol)
 
-                                                Globals.lastMove.value = LastMove(
-                                                    from = Pair(selCol, selRow),
-                                                    to = Pair(clickedCol, clickedRow)
-                                                )
+                                                val movedPiece = ChessBoardActionHandler.movePiece(clickedRow, clickedCol, selRow, selCol)
 
-
-                                                Globals.chessBoard[clickedRow][clickedCol].pieceOnBox = Globals.chessBoard[selRow][selCol].pieceOnBox
-                                                Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!.position = Pair(clickedCol, clickedRow)
-                                                Globals.chessBoard[selRow][selCol].pieceOnBox = null
-
-
-                                                val movedPiece = Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!
-
-                                                PromotionHandler.verifyPromotion(movedPiece)
-
-                                                if (Globals.isWhitePromotion.value) {
-                                                    PromotionHandler.preparePromotionSelectionWhite(movedPiece)
-                                                } else if (Globals.isBlackPromotion.value) {
-                                                    PromotionHandler.preparePromotionSelectionBlack(movedPiece)
-                                                }else{
-                                                    Globals.lastMove.value = LastMove(
-                                                        from = Pair(selCol, selRow),
-                                                        to = Pair(clickedCol, clickedRow)
-                                                    )
-                                                    Globals.isWhiteMove.value = !Globals.isWhiteMove.value
-                                                }
+                                                ChessBoardActionHandler.makePromotionOrCompleteMove(movedPiece, clickedRow, clickedCol, selRow, selCol)
 
                                                 // Castle
-                                                CastleHandler.disableCastleIfKingOrRookMoved(movedPiece = movedPiece)
-
-                                                CastleHandler.moveRookOnCastle(
-                                                    movedPiece = movedPiece,
-                                                    selCol = selCol,
-                                                    selRow = selRow,
-                                                    clickedRow = clickedRow,
-                                                    clickedCol = clickedCol,
-                                                )
+                                                CastleHandler.disableCastleIfKingOrRookMoved(movedPiece)
+                                                CastleHandler.moveRookOnCastle(movedPiece, selRow, selCol, clickedRow,clickedCol)
                                             }
                                         }
                                     }else{
@@ -119,44 +69,13 @@ fun ChessBoard() {
                                     }
 
                                     if(!promoteDone || promoteCanceled) {
-                                        //Quitar selección
-                                        Globals.possibleMoves.value = emptyList()
-                                        Globals.chessBoard.forEach { row ->
-                                            row.forEach { box ->
-                                                box.pieceOnBox?.isSelected?.value = false
-                                            }
-                                        }
+                                        ChessBoardActionHandler.removePieceSelection()
 
-
-                                        // Seleccionar pieza si hay una en la casilla clicada
                                         if(!moveWasDone) {
-                                            if (Globals.chessBoard[clickedRow][clickedCol].pieceOnBox != null) {
-                                                if(
-                                                    (Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!.color == org.pruden.tablero.models.Color.White && Globals.isWhiteMove.value)
-                                                    || (Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!.color == org.pruden.tablero.models.Color.Black && !Globals.isWhiteMove.value)
-                                                ) {
-                                                    Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!.isSelected.value = true
-                                                    Globals.possibleMoves.value = MoveCalculator.getPosibleMoves(
-                                                        Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!
-                                                    )
-                                                }
-                                            }
+                                            ChessBoardActionHandler.tryToSelectAPiece(clickedRow, clickedCol)
                                         }
 
-                                        // Comprobar si hay jaque
-                                        if (Globals.isWhiteMove.value) {
-                                            val attacked = MoveCalculator.calculateCellsControledByOponent(org.pruden.tablero.models.Color.White)
-                                            val wKing = CellHandler.getKingByColor(org.pruden.tablero.models.Color.White)
-                                            val inCheck = attacked.contains(wKing.position)
-                                            Globals.whiteIsChecked.value = inCheck
-                                            Globals.checkedKingPos.value = if (inCheck) wKing.position else null
-                                        } else {
-                                            val attacked = MoveCalculator.calculateCellsControledByOponent(org.pruden.tablero.models.Color.Black)
-                                            val bKing = CellHandler.getKingByColor(org.pruden.tablero.models.Color.Black)
-                                            val inCheck = attacked.contains(bKing.position)
-                                            Globals.blackIsChecked.value = inCheck
-                                            Globals.checkedKingPos.value = if (inCheck) bKing.position else null
-                                        }
+                                        ChessBoardActionHandler.verifyIfCheck()
                                     }
                                 },
                                 onDrag = { x, y, offSet ->
--- composeApp/src/jvmMain/kotlin/org/pruden/tablero/utils/ChessBoardActionHandler.kt
+++ composeApp/src/jvmMain/kotlin/org/pruden/tablero/utils/ChessBoardActionHandler.kt
@@ -1,6 +1,105 @@
 package org.pruden.tablero.utils
 
+import org.pruden.tablero.globals.Globals
+import org.pruden.tablero.models.LastMove
+import org.pruden.tablero.models.Piece
+
 object ChessBoardActionHandler {
+    fun removePieceSelection(){
+        Globals.possibleMoves.value = emptyList()
+        Globals.chessBoard.forEach { row ->
+            row.forEach { box ->
+                box.pieceOnBox?.isSelected?.value = false
+            }
+        }
+    }
 
+    fun movePiece(
+        clickedRow: Int,
+        clickedCol: Int,
+        selRow: Int,
+        selCol: Int,
+    ): Piece{
+        Globals.chessBoard[clickedRow][clickedCol].pieceOnBox = Globals.chessBoard[selRow][selCol].pieceOnBox
+        Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!.position = Pair(clickedCol, clickedRow)
+        Globals.chessBoard[selRow][selCol].pieceOnBox = null
+        return Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!
+    }
 
+    fun saveLastMove(
+        clickedRow: Int,
+        clickedCol: Int,
+        selRow: Int,
+        selCol: Int,
+    ){
+        Globals.lastPieceStartPos = Globals.chessBoard[selRow][selCol].pieceOnBox!!.position
+        Globals.lastMove.value = LastMove(
+            from = Pair(selCol, selRow),
+            to = Pair(clickedCol, clickedRow)
+        )
+    }
+
+    fun getSelectedPiecePos(): Pair<Int, Int>? {
+        return Globals.chessBoard
+            .flatMapIndexed { rowIndex, row ->
+                row.mapIndexedNotNull { colIndex, box ->
+                    if (box.pieceOnBox?.isSelected?.value == true) Pair(colIndex, rowIndex) else null
+                }
+            }.firstOrNull()
+    }
+
+    fun makePromotionOrCompleteMove(
+        movedPiece: Piece,
+        clickedRow: Int,
+        clickedCol: Int,
+        selRow: Int,
+        selCol: Int,
+    ){
+        PromotionHandler.verifyPromotion(movedPiece)
+
+        if (Globals.isWhitePromotion.value) {
+            PromotionHandler.preparePromotionSelectionWhite(movedPiece)
+        } else if (Globals.isBlackPromotion.value) {
+            PromotionHandler.preparePromotionSelectionBlack(movedPiece)
+        }else{
+            Globals.lastMove.value = LastMove(
+                from = Pair(selCol, selRow),
+                to = Pair(clickedCol, clickedRow)
+            )
+            Globals.isWhiteMove.value = !Globals.isWhiteMove.value
+        }
+    }
+
+    fun tryToSelectAPiece(
+        clickedRow: Int,
+        clickedCol: Int,
+    ){
+        if (Globals.chessBoard[clickedRow][clickedCol].pieceOnBox != null) {
+            if(
+                (Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!.color == org.pruden.tablero.models.Color.White && Globals.isWhiteMove.value)
+                || (Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!.color == org.pruden.tablero.models.Color.Black && !Globals.isWhiteMove.value)
+            ) {
+                Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!.isSelected.value = true
+                Globals.possibleMoves.value = MoveCalculator.getPosibleMoves(
+                    Globals.chessBoard[clickedRow][clickedCol].pieceOnBox!!
+                )
+            }
+        }
+    }
+
+    fun verifyIfCheck(){
+        if (Globals.isWhiteMove.value) {
+            val attacked = MoveCalculator.calculateCellsControledByOponent(org.pruden.tablero.models.Color.White)
+            val wKing = CellHandler.getKingByColor(org.pruden.tablero.models.Color.White)
+            val inCheck = attacked.contains(wKing.position)
+            Globals.whiteIsChecked.value = inCheck
+            Globals.checkedKingPos.value = if (inCheck) wKing.position else null
+        } else {
+            val attacked = MoveCalculator.calculateCellsControledByOponent(org.pruden.tablero.models.Color.Black)
+            val bKing = CellHandler.getKingByColor(org.pruden.tablero.models.Color.Black)
+            val inCheck = attacked.contains(bKing.position)
+            Globals.blackIsChecked.value = inCheck
+            Globals.checkedKingPos.value = if (inCheck) bKing.position else null
+        }
+    }
 }
